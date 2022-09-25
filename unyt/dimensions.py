2f4961465dc357829364fce27b9a2ae0baee7589
John ZuHone <jzuhone@gmail.com>
2022-09-25 19:51:10 -0400

    Some initial work on memory units

--- unyt/dimensions.py
+++ unyt/dimensions.py
@@ -28,6 +28,8 @@ luminous_intensity = Symbol("(luminous_intensity)", positive=True)
 dimensionless = sympify(1)
 #: logarithmic
 logarithmic = Symbol("(logarithmic)", positive=True)
+#: memory
+memory = Symbol("(memory)", positive=True)
 
 #: A list of all of the base dimensions
 base_dimensions = [
@@ -40,6 +42,7 @@ base_dimensions = [
     dimensionless,
     luminous_intensity,
     logarithmic,
+    memory,
 ]
 
 #
